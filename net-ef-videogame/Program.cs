93796a562b0d3fbf19ea401f673146f2aa869e4d
giovannitamburini <giovatambu@gmail.com>
2023-09-22 15:55:38 +0200

    completed first query (add software house)

--- net-ef-videogame/Program.cs
+++ net-ef-videogame/Program.cs
@@ -32,54 +32,167 @@
             //};
 
             // mi creo delle nuove Software House
-            SoftwareHouse firstSoftwareHouse = new SoftwareHouse()
-            {
-                Name = "Sony Interactive Entertainment",
-                TaxId = "120338SKJDD192",
-                City = "Tokyo",
-                Country = "Japan",
-                Videogames = new List<Videogame>()
-            };
+            //SoftwareHouse firstSoftwareHouse = new SoftwareHouse()
+            //{
+            //    Name = "Sony Interactive Entertainment",
+            //    TaxId = "120338SKJDD192",
+            //    City = "Tokyo",
+            //    Country = "Japan",
+            //    Videogames = new List<Videogame>()
+            //};
 
-            SoftwareHouse secondSoftwareHouse = new SoftwareHouse()
-            {
-                Name = "Activision Blizzard",
-                TaxId = "AAA392840294223",
-                City = "Santa Monica",
-                Country = "Califoria",
-                Videogames = new List<Videogame>()
-            };
+            //SoftwareHouse secondSoftwareHouse = new SoftwareHouse()
+            //{
+            //    Name = "Activision Blizzard",
+            //    TaxId = "AAA392840294223",
+            //    City = "Santa Monica",
+            //    Country = "Califoria",
+            //    Videogames = new List<Videogame>()
+            //};
 
-            SoftwareHouse thirdSoftwareHouse = new SoftwareHouse()
-            {
-                Name = "Epic Games",
-                TaxId = "BBB04790938402",
-                City = "Cary",
-                Country = "North Carolina",
-                Videogames = new List<Videogame>()
-            };
+            //SoftwareHouse thirdSoftwareHouse = new SoftwareHouse()
+            //{
+            //    Name = "Epic Games",
+            //    TaxId = "BBB04790938402",
+            //    City = "Cary",
+            //    Country = "North Carolina",
+            //    Videogames = new List<Videogame>()
+            //};
 
-            //aggiungo le software house al databse
-            using (VideogameContext db = new VideogameContext())
+            ////aggiungo le software house al databse
+            //using (VideogameContext db = new VideogameContext())
+            //{
+            //    try
+            //    {
+            //        db.Add(firstSoftwareHouse);
+            //        db.Add(secondSoftwareHouse);
+            //        db.Add(thirdSoftwareHouse);
+
+            //        db.SaveChanges();
+
+            //        Console.WriteLine("Le Software House sono state aggiunte con successo");
+
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine(ex.Message);
+            //    }
+
+
+            //}
+
+
+            bool repeatMenu = true;
+
+            while (repeatMenu)
             {
-                try
+
+
+                Console.WriteLine(@"
+1 - inserire una nuova software house
+2 - inserire un nuovo videogioco
+3 - ricercare un videogioco per id
+4 - ricercare tutti i videogiochi aventi il nome contenente una determinata stringa inserita in input
+5 - cancellare un videogioco
+6 - chiudere il programma
+");
+
+                Console.Write("Prego seleziona l'opzione che desideri effettuare: ");
+                int selectedOption = int.Parse(Console.ReadLine());
+
+
+                switch (selectedOption)
                 {
-                    db.Add(firstSoftwareHouse);
-                    db.Add(secondSoftwareHouse);
-                    db.Add(thirdSoftwareHouse);
+                    case 1:
 
-                    Console.WriteLine("Le Software House sono state aggiunte con successo");
+                        Console.WriteLine("Hai selezionato l'opzione 1: inserire una nuova software house");
 
-                } catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
+                        Console.WriteLine("Inserisci i dati della software house");
+
+                        Console.Write("Inserisci il nome della software house: ");
+                        string name = Console.ReadLine();
+
+                        Console.Write("Inserisci la taxId della software house: ");
+                        string taxId = Console.ReadLine();
+
+                        Console.Write("Inserisci la città della sede della sh: ");
+                        string city = Console.ReadLine();
+
+                        Console.Write("Inserisci il paese della sede della sh: ");
+                        string country = Console.ReadLine();
+
+
+
+                        SoftwareHouse newSoftwareHouse = new SoftwareHouse()
+                        {
+                            Name = name,
+                            TaxId = taxId,
+                            City = city,
+                            Country = country,
+                            Videogames = new List<Videogame>()
+                        };
+
+
+                        using (VideogameContext db = new VideogameContext())
+                        {
+                            try
+                            {
+                                db.Add(newSoftwareHouse);
+                                
+                                db.SaveChanges();
+
+                                Console.WriteLine("La Software House è stata aggiuntea con successo");
+
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                            }
+
+
+                        }
+
+                        break;
+
+                    case 2:
+
+                        Console.WriteLine("Hai selezionato l'opzione 2 - inserire un nuovo videogioco");
+
+
+                        break;
+
+                    case 3:
+
+                        Console.WriteLine("Hai selezionato l'opzione 2: ricercare un videogioco per id");
+
+                        break;
+
+                    case 4:
+
+                        Console.WriteLine("Hai selezionato l'opzione 3: ricercare tutti i videogiochi aventi il nome contenente una determinata stringa inserita in input");
+
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Hai selezionato l'opzione 4: cancellare un videogioco");
+
+                        break;
+
+                    case 6:
+
+                        Console.WriteLine("Hai selezionato l'opzione 5: chiudere il programma");
+
+                        repeatMenu = false;
+
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Non hai selezionato un opzione valida");
+
+                        break;
                 }
-                
-
-                db.SaveChanges();
             }
-
-
 
 
         }

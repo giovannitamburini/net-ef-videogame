de0044d6ae0b8a2e3426bed598b208fd043e2cd6
giovannitamburini <giovatambu@gmail.com>
2023-09-22 14:37:54 +0200

    added Videogame class & SoftwareHouse class

---
+++ net-ef-videogame/Videogame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame
+{
+    public class Videogame
+    {
+        public long VideogameId { get; set; }
+        public string Name { get; set; }
+        public string Overview { get; set; }
+        public DateTime Release_date { get; set; }
+        
+        public long SoftwareHouseId { get; set; }
+        public SoftwareHouse SoftwareHouse { get; set; }
+        
+
+        //public override string ToString()
+        //{
+        //    return $"id: {VideogameId} - nome: {Name} - overview: {Overview} - release date: {Release_date} - software house id: ";
+        //}
+    }
+}
